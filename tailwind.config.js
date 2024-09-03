1556f47cf48aed3882bd35598cafcd94013574e3
paul <ophantomo990@gmail.com>
2024-09-03 00:50:33 -0700

    Inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './dist/*.html',
+    './dist/**/*.html',
+    './dist/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+  ],
+}
